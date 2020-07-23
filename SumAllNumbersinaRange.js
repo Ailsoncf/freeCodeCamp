b877830952791ab0446c0918b8bf654095700cf5
Ailsoncf <ailson_cf@yahoo.com.br>
2020-07-23 22:35:26 +1200

    Sum All Numbers in a Range

--- SumAllNumbersinaRange.js
+++ SumAllNumbersinaRange.js
@@ -1,6 +1,7 @@
 function sumAll(arr) {
-  const max = arr.reduce((a, b) => (a > b ? a : b))
-  const min = arr.reduce((a, b) => (a < b ? a : b))
+  const a = [1, 4]
+  const max = Math.max.apply(null, arr) //arr.reduce((a, b) => (a > b ? a : b))
+  const min = Math.min.apply(null, arr) //arr.reduce((a, b) => (a < b ? a : b))
   let sum = 0
   for (i = min; i <= max; i++) {
     sum += i
