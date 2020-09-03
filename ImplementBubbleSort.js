571831d7f6092d6145fe24d9f50b24956f06ba47
ailsoncf <ailson_cf@yahoo.com.br>
2020-09-04 11:18:57 +1200

    Implement Bubble Sort

--- ImplementBubbleSort.js
+++ ImplementBubbleSort.js
@@ -1,7 +1,7 @@
 function bubbleSort(array) {
   const arr = [...array]
-  for (let i = 1; i < arr.length; i += 1) {
-    for (let j = 0; j < arr.length - i; j += 1) {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
       if (arr[j] > arr[j + 1]) {
         ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
       }
